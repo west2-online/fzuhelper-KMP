7e8f553940bee80e685a0e33ec6761577bff3f11
“沈轻腾“ <sqt18750016193@163.com>
2024-03-22 22:57:18 +0800

    feat:重构漏下的的网络api

--- shared/src/commonMain/kotlin/data/Report/reportData/ReportResponse.kt
+++ shared/src/commonMain/kotlin/data/Report/reportData/ReportResponse.kt
@@ -1,11 +1,21 @@
 package data.Report.reportData
 
 import kotlinx.serialization.Serializable
+import util.network.NetworkResult
+import util.network.networkErrorWithLog
 
 @Serializable
 data class ReportResponse(
     val code: Int,
     val `data`: String?,
     val msg: String?
-)
-
+){
+    fun toNetworkResult(): NetworkResult<String> {
+        return when(code){
+            1 -> networkErrorWithLog(code,"举报失败")
+            0 -> networkErrorWithLog(code,"信心不足")
+            2 -> NetworkResult.Success("举报成功")
+            else -> networkErrorWithLog(code,"举报失败")
+        }
+    }
+}
--- shared/src/commonMain/kotlin/ui/compose/Report/ReportViewModel.kt
+++ shared/src/commonMain/kotlin/ui/compose/Report/ReportViewModel.kt
@@ -1,16 +1,14 @@
 package ui.compose.Report
 
-import data.Report.reportData.ReportResponse
 import dev.icerock.moko.mvvm.flow.CMutableStateFlow
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import repository.ReportRepository
-import repository.ReportStatus
-import util.flow.catchWithMassage
-import util.flow.collectWithMassage
+import util.flow.actionWithLabel
 import util.flow.launchInDefault
 import util.network.NetworkResult
+import util.network.networkErrorWithLog
 import util.network.resetWithLog
 
 class ReportViewModel(
@@ -27,42 +25,37 @@ class ReportViewModel(
     fun reportComment(commentId:String,typeId:Int,postId:String){
         viewModelScope.launchInDefault {
             repository.reportComment(commentId, typeId, postId)
-                .catchWithMassage {
-                    _reportCommentResponse.resetWithLog(NetworkResult.Error(Throwable("举报失败")))
-                }.collectWithMassage {
-                    _reportCommentResponse.resetWithLog(it.toNetworkResult())
-                }
+                .actionWithLabel(
+                    "reportComment/reportComment",
+                    catchAction = { label, error ->
+                        _reportCommentResponse.resetWithLog(label, networkErrorWithLog(error,"举报失败"))
+                    },
+                    collectAction = { label, data ->
+                        _reportCommentResponse.resetWithLog(label,data.toNetworkResult())
+                    }
+                )
         }
     }
+
+
+
+
 
     fun reportPost(typeId:Int,postId:String){
         viewModelScope.launchInDefault {
             repository.reportPost(typeId, postId)
-                .catchWithMassage {
-                    _reportPostResponse.resetWithLog(NetworkResult.Error(Throwable("举报失败")))
-                }.collectWithMassage {
-                    _reportPostResponse.resetWithLog(it.toNetworkResult())
-                }
+                .actionWithLabel(
+                    "reportPost/reportPost",
+                    catchAction = { label, error ->
+                        _reportPostResponse.resetWithLog(label, networkErrorWithLog(error,"举报失败"))
+                    },
+                    collectAction = { label, data ->
+                        _reportPostResponse.resetWithLog(label,data.toNetworkResult())
+                    }
+                )
+
         }
     }
 }
 
-fun ReportResponse.toNetworkResult(): NetworkResult<String> {
-    val data = ReportStatus.values().find {
-        it.value == this.code
-    }
-    return when(data){
-        null -> {
-            NetworkResult.Error(Throwable("举报失败"))
-        }
-        ReportStatus.InsufficientInformation -> {
-            NetworkResult.Success("信息不足")
-        }
-        ReportStatus.TheReportWasSuccessful -> {
-            NetworkResult.Success("举报成功")
-        }
-        else -> {
-            NetworkResult.Error(Throwable("举报失败"))
-        }
-    }
-}
+
