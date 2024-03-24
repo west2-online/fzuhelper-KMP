4c21128a6db8a6bc319dfe6759922d13b5866926
“沈轻腾“ <sqt18750016193@163.com>
2024-03-25 02:29:12 +0800

    fix: 修复了drawer的手势问题

--- shared/src/commonMain/kotlin/ui/compose/Main/Main.kt
+++ shared/src/commonMain/kotlin/ui/compose/Main/Main.kt
@@ -89,7 +89,7 @@ object Main : Screen{
             val scope = rememberCoroutineScope()
             val scaffoldState = rememberScaffoldState()
             Scaffold(
-                drawerGesturesEnabled = false ,
+                drawerGesturesEnabled = scaffoldState.drawerState.isOpen ,
                 content = {
                     Column (
                         modifier = Modifier
