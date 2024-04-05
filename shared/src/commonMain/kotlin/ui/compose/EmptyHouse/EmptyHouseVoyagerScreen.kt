59ba5997054ffecd0dbd5834d145a0038bd98aa5
“沈轻腾“ <sqt18750016193@163.com>
2024-04-05 23:56:04 +0800

    feat：修改部分空教室视图

--- shared/src/commonMain/kotlin/ui/compose/EmptyHouse/EmptyHouseVoyagerScreen.kt
+++ shared/src/commonMain/kotlin/ui/compose/EmptyHouse/EmptyHouseVoyagerScreen.kt
@@ -4,20 +4,29 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
@@ -28,12 +37,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
@@ -52,7 +63,9 @@ import util.compose.parentSystemControl
 class EmptyHouseVoyagerScreen(
     val parentPaddingControl: ParentPaddingControl = defaultSelfPaddingControl()
 ) :Screen{
-    @OptIn(ExperimentalMaterial3Api::class, FormatStringsInDatetimeFormats::class)
+    @OptIn(ExperimentalMaterial3Api::class, FormatStringsInDatetimeFormats::class,
+        ExperimentalLayoutApi::class
+    )
     @Composable
     override fun Content() {
         val startClass = remember {
@@ -69,35 +82,99 @@ class EmptyHouseVoyagerScreen(
         val selectDate = remember {
             mutableStateOf(false)
         }
+        val selectCampus = remember {
+            mutableStateOf(campusList.first())
+        }
 
         val scaffoldState = rememberScaffoldState()
         val date = rememberDatePickerState(
             initialSelectedDateMillis = Clock.System.todayIn(TimeZone.UTC).atStartOfDayIn(TimeZone.UTC).toEpochMilliseconds()
         )
+        val scope = rememberCoroutineScope()
         Scaffold(
             modifier = Modifier,
             content = {
                   Box(
                       modifier = Modifier
+                          .fillMaxSize()
                           .parentSystemControl(parentPaddingControl)
+                          .padding(horizontal = 10.dp)
                   ){
-                      Button(
-                          onClick = {
-                              selectDate.value = true
-                          }
+                      Column (
+                          modifier = Modifier
+                              .fillMaxSize()
                       ){
-                          Text(date.selectedDateMillis?.let { it1 ->
-                              val dateTime = Instant.fromEpochMilliseconds(
-                                  it1
-                              ).toLocalDateTime(TimeZone.UTC)
-
-                              return@let dateTime.toInstant(TimeZone.UTC)
-                                  .toLocalDateTime(TimeZone.UTC).date.format(LocalDate.Format {
-                                      byUnicodePattern(
-                                          "yyyy-MM-dd"
+                          FlowRow(
+                              horizontalArrangement = Arrangement.spacedBy(20.dp)
+                          ) {
+                              Button(
+                                  onClick = {
+                                      scope.launch {
+                                          scaffoldState.drawerState.open()
+                                      }
+                                  },
+                                  content = {
+                                      Text(
+                                          "第 ${startClass.value} 节课 -> 第 ${endClass.value} 节课",
+                                          modifier = Modifier,
+                                          textAlign = TextAlign.Center
                                       )
-                                  })
-                          }.toString())
+                                  }
+                              )
+                              Button(
+                                  onClick = {
+                                      selectDate.value = true
+                                  }
+                              ){
+                                  Text(date.selectedDateMillis?.let { it1 ->
+                                      val dateTime = Instant.fromEpochMilliseconds(
+                                          it1
+                                      ).toLocalDateTime(TimeZone.UTC)
+
+                                      return@let dateTime.toInstant(TimeZone.UTC)
+                                          .toLocalDateTime(TimeZone.UTC).date.format(LocalDate.Format {
+                                              byUnicodePattern(
+                                                  "yyyy-MM-dd"
+                                              )
+                                          })
+                                  }.toString())
+                              }
+                              Box {
+                                  val expanded = remember {
+                                      mutableStateOf(false)
+                                  }
+                                  Button(
+                                      onClick = {
+                                          scope.launch {
+                                             expanded.value = true
+                                          }
+                                      },
+                                      content = {
+                                          Text(
+                                              selectCampus.value
+                                          )
+                                      }
+                                  )
+                                  DropdownMenu(
+                                      expanded = expanded.value,
+                                      onDismissRequest = {
+                                          expanded.value = false
+                                      },
+                                      content = {
+                                          campusList.forEach {
+                                              DropdownMenuItem(
+                                                  onClick = {
+                                                      selectCampus.value = it
+                                                      expanded.value = false
+                                                  }
+                                              ){
+                                                  Text(it)
+                                              }
+                                          }
+                                      }
+                                  )
+                              }
+                          }
                       }
                       if (selectDate.value){
                           DatePickerDialog(
@@ -127,6 +204,19 @@ class EmptyHouseVoyagerScreen(
                                   showModeToggle = false
                               )
                           }
+                      }
+                      FloatingActionButton(
+                          onClick = {
+
+                          },
+                          modifier = Modifier
+                              .align(Alignment.BottomEnd)
+                              .offset((-10).dp, (-10).dp)
+                      ){
+                          Icon(
+                              imageVector = Icons.Filled.Refresh,
+                              contentDescription = null
+                          )
                       }
                   }
             },
@@ -210,15 +300,21 @@ class EmptyHouseVoyagerScreen(
                         }
                     }
                 }
-
             },
-            scaffoldState = scaffoldState
+            scaffoldState = scaffoldState,
+            drawerGesturesEnabled = scaffoldState.drawerState.isOpen
         )
     }
 }
 
 
-@Composable
-fun EmptyHorseFilter(){
 
-}
+
+
+val campusList = listOf(
+    "旗山校区",
+    "泉港校区",
+    "铜盘校区",
+    "厦门工艺美院",
+    "怡山校区",
+)
