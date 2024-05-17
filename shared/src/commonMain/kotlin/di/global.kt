9bb1ce0e104fe4c2bd9b038815c94d8c2bb5c15c
“沈轻腾“ <sqt18750016193@163.com>
2024-05-18 00:18:09 +0800

    feat: 添加di目录的注释

--- shared/src/commonMain/kotlin/di/global.kt
+++ shared/src/commonMain/kotlin/di/global.kt
@@ -4,7 +4,16 @@ import createDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 
+/**
+ * Database 获取底层数据库
+ */
 val database = createDatabase()
 
+
 internal val job = Job()
+
+/**
+ * Global scope
+ * 全局可用的协程
+ */
 val globalScope = CoroutineScope(job)
