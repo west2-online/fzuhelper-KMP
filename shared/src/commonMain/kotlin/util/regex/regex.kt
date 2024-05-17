9b1077aeeaeff74506023e44aa267d19e32c6114
“沈轻腾“ <sqt18750016193@163.com>
2024-05-18 02:05:16 +0800

    feat: 添加util/regex/regex.kt的注释

--- shared/src/commonMain/kotlin/util/regex/regex.kt
+++ shared/src/commonMain/kotlin/util/regex/regex.kt
@@ -1,16 +1,30 @@
 package util.regex
 
+/**
+ * 匹配邮箱
+ * @param email String
+ * @return Boolean
+ */
 fun matchEmail(email:String):Boolean{
     val emailRegex = Regex("[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+")
     return emailRegex.matches(email)
 }
 
-@Suppress
+/**
+ * 匹配手机好
+ * @param phone String
+ * @return Boolean
+ */
 fun matchPhone(phone:String):Boolean{
     val phoneRegex = Regex("/^1[3456789]\\d{9}\$/")
     return phoneRegex.matches(phone)
 }
 
+/**
+ * 匹配http 的 url
+ * @param url String
+ * @return Boolean
+ */
 fun matchHttpUrl(url:String):Boolean{
     val httpRegex = Regex("/^(?:(http|https|ftp):\\/\\/)?((|[\\w-]+\\.)+[a-z0-9]+)(?:(\\/[^/?#]+)*)?(\\?[^#]+)?(#.+)?\$/i;")
     return httpRegex.matches(url)
