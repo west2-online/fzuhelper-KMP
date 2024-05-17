6c6208b7e68c0e7e7374bfddb4604b1525b85557
“沈轻腾“ <sqt18750016193@163.com>
2024-05-18 01:36:15 +0800

    feat: 添加repository/RibbonRepository.kt的注释

--- shared/src/commonMain/kotlin/repository/RibbonRepository.kt
+++ shared/src/commonMain/kotlin/repository/RibbonRepository.kt
@@ -7,9 +7,18 @@ import io.ktor.client.request.get
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
+/**
+ * 轮播页的仓库层
+ * @property client HttpClient
+ * @constructor
+ */
 class RibbonRepository(
     private val client: HttpClient
 ) {
+    /**
+     * 获取已有轮播图列表
+     * @return Flow<RibbonList>
+     */
     fun getRibbonList():Flow<RibbonList>{
         return flow {
             val response = client.get("/ribbon/list").body<RibbonList>()
