e22fdc8e1c5833a5928a911ddbe2a23b0a1a4c53
“沈轻腾“ <sqt18750016193@163.com>
2024-07-11 16:07:42 +0800

    fix: 修复了教务处网页的异常显示

--- shared/src/commonMain/kotlin/ui/compose/UndergraduateWebView/UndergraduateWebViewViewModel.kt
+++ shared/src/commonMain/kotlin/ui/compose/UndergraduateWebView/UndergraduateWebViewViewModel.kt
@@ -7,7 +7,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import com.multiplatform.webview.web.LoadingState
@@ -15,12 +17,16 @@ import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewNavigator
 import com.multiplatform.webview.web.rememberWebViewState
 import config.JWCH_BASE_URL
+import io.github.koalaplot.core.xygraph.AnchorPoint
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
+import util.compose.ErrorText
 import util.compose.ParentPaddingControl
 import util.compose.defaultSelfPaddingControl
 import util.compose.parentSystemControl
 import util.network.CollectWithContent
+import util.network.CollectWithContentInBox
 import kotlin.jvm.Transient
 
 
@@ -51,6 +57,7 @@ fun OwnWebViewScreen(
 ) {
     val viewModel: UndergraduateWebViewViewModel = koinInject()
     val clientState = viewModel.clientState.collectAsState()
+    val scope = rememberCoroutineScope()
     LaunchedEffect(Unit) {
         viewModel.getClassScheduleClient()
     }
@@ -58,7 +65,7 @@ fun OwnWebViewScreen(
     val isLoadingCookie = remember {
         mutableStateOf(true)
     }
-    clientState.CollectWithContent(
+    clientState.CollectWithContentInBox(
         success = {
             url = start.replace("{id}", it.id)
             val state = rememberWebViewState(url,mapOf("User-Agent" to "Mozilla/5.0 (Linux; Android 9; ELE-AL00 Build/HUAWEIELE-AL0001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/63.0.3239.83 Mobile Safari/537.36 T7/11.15 baiduboxapp/11.15.5.10 (Baidu; P1 9)"))
@@ -88,8 +95,7 @@ fun OwnWebViewScreen(
                 WebView(
                     state,
                     modifier = Modifier
-                        .fillMaxSize()
-                        .parentSystemControl(parentPaddingControl),
+                        .fillMaxSize(),
                     navigator = webNavigation
                 )
             }else{
@@ -97,7 +103,25 @@ fun OwnWebViewScreen(
             }
         },
         loading = {
-            CircularProgressIndicator()
-        }
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center)
+            )
+        },
+        error = {
+            ErrorText(
+                text = "加载失败",
+                onClick = {
+                    scope.launch{
+                        viewModel.getClassScheduleClient()
+                    }
+                },
+                boxModifier = Modifier
+                    .fillMaxSize()
+            )
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .parentSystemControl(parentPaddingControl),
     )
 }
